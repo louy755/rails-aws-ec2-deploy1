46726f7be5da1904f69813975a415c803862d6fe
louy755 <lumberjack1695@yahoo.com>
2017-07-29 18:14:31 -0500

    before deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '35.163.24.233', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
